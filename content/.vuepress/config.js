8567426023daf2c990bb623c92cf812a3acfd286
Thorben Dittmar <roikiermedia@users.noreply.github.com>
2022-11-07 22:32:04 +0100

    perf: removed loading of unused fonts

--- content/.vuepress/config.js
+++ content/.vuepress/config.js
@@ -7,10 +7,6 @@ module.exports = {
     }
   },
   head: [
-    ['link', { rel: 'preload', href: '/fonts/space-mono-v4-latin/space-mono-v4-latin-regular.woff2', as: 'font', type: 'font/woff2', crossorigin: "anonymous" }],
-    ['link', { rel: 'preload', href: '/fonts/space-mono-v4-latin/space-mono-v4-latin-italic.woff2', as: 'font', type: 'font/woff2', crossorigin: "anonymous" }],
-    ['link', { rel: 'preload', href: '/fonts/space-mono-v4-latin/space-mono-v4-latin-700.woff2', as: 'font', type: 'font/woff2', crossorigin: "anonymous" }],
-    ['link', { rel: 'preload', href: '/fonts/space-mono-v4-latin/space-mono-v4-latin-700italic.woff2', as: 'font', type: 'font/woff2', crossorigin: "anonymous" }],
     ['link', {rel: "icon", sizes: "32x32", href: "/favicon/favicon-32x32.png"}],
     ['link', {rel: "icon", sizes: "16x16", href: "/favicon/favicon-16x16.png"}],
     ['link', {rel: "icon", sizes: "180x180", href: "/favicon/apple-touch-icon.png"}],
