8489fe3d6caca91a043a1dadf6be5617bf6a8459
adb-sh <git@adb.sh>
2023-05-04 20:43:05 +0200

    update links

--- api/index.mjs
+++ api/index.mjs
@@ -5,10 +5,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(express.urlencoded({ extended:false }));
+app.use(express.urlencoded({ extended: false }));
 
 const axiosZammad = axios.create({
-  baseURL: 'https://zammad.chaostreff-flensburg.de/api/v1',
+  baseURL: "https://zammad.chaostreff-flensburg.de/api/v1",
   auth: {
     username: process.env.AUTH_USERNAME,
     password: process.env.AUTH_PASSWORD,
@@ -16,78 +16,86 @@ const axiosZammad = axios.create({
 });
 
 const requiredFields = [
-  'name',
-  'email',
-  'hochschule',
-  'startdate',
-  'thesisName',
-  'thesisTask',
-  'reasonApplicationThesis',
-  'reasonApplicationPerson',
+  "name",
+  "email",
+  "hochschule",
+  "startdate",
+  "thesisName",
+  "thesisTask",
+  "reasonApplicationThesis",
+  "reasonApplicationPerson",
 ];
 
-app.post('/api/form', async (req, res) => {
-  if (req.body.captcha?.toLowerCase() !== 'flensburg') {
+app.post("/api/form", async (req, res) => {
+  if (req.body.captcha?.toLowerCase() !== "flensburg") {
     res.status(401);
-    res.send('wrong_captcha');
+    res.send("wrong_captcha");
     return;
   }
-  if (req.body.approval?.toLowerCase() !== 'on') {
+  if (req.body.approval?.toLowerCase() !== "on") {
     res.status(401);
-    res.send('please_give_approval');
+    res.send("please_give_approval");
     return;
   }
-  const requiredFieldsMissing = requiredFields.filter(element => !req.body?.[element]);
+  const requiredFieldsMissing = requiredFields.filter((element) =>
+    !req.body?.[element]
+  );
   if (requiredFieldsMissing.length > 0) {
     res.status(400);
-    res.send(`missing_required_fields: ${ requiredFieldsMissing.join(', ') }`);
+    res.send(`missing_required_fields: ${requiredFieldsMissing.join(", ")}`);
     return;
   }
 
   try {
-    const names = req.body.name.split(' ');
-    await axiosZammad.post('/users', {
+    const names = req.body.name.split(" ");
+    await axiosZammad.post("/users", {
       lastname: names.pop(),
-      firstname: names.join(' '),
+      firstname: names.join(" "),
       email: req.body.email,
-      organization: 'Chaostreff Flensburg',
-      roles: ['Customer'],
+      organization: "Chaostreff Flensburg",
+      roles: ["Customer"],
     });
   } catch (e) {
-    if (e.response.data.includes('Email address is already used for other user.')) {
-      console.error(`user "${ req.body.email }" does already exit`);
+    if (
+      e.response.data.includes("Email address is already used for other user.")
+    ) {
+      console.error(`user "${req.body.email}" does already exit`);
     } else {
       console.error(e);
       res.status(500);
-      res.send('Beim Erstellen des Benutzers ist ein Fehler aufgetreten. Wenn der Fehler noch mal auftritt, solltest du dich an ccs@chaostreff-flensburg.de melden.');
+      res.send(
+        "Beim Erstellen des Benutzers ist ein Fehler aufgetreten. Wenn der Fehler noch mal auftritt, solltest du dich an ccs@chaostreff-flensburg.de melden.",
+      );
       return;
     }
   }
 
   try {
-    let body = ''
-    Object.entries(req.body).forEach(element=>{
-      body += `${element[0]}: \n ${element[1]} \n\n`
-    })
-    body += JSON.stringify(req.body)
-    await axiosZammad.post('/tickets', {
-      title: `CCS Bewerbung - ${ req.body.name } - ${ req.body.thesisName }`,
+    let body = "";
+    Object.entries(req.body).forEach((element) => {
+      body += `${element[0]}: \n ${element[1]} \n\n`;
+    });
+    body += JSON.stringify(req.body);
+    await axiosZammad.post("/tickets", {
+      title: `CCS Bewerbung - ${req.body.name} - ${req.body.thesisName}`,
       customer: req.body.email,
-      group: 'Users',
+      group: "Users",
       article: {
-        subject: `CCS Bewerbung - ${ req.body.name } - ${ req.body.thesisName }`,
+        subject: `CCS Bewerbung - ${req.body.name} - ${req.body.thesisName}`,
         body,
-        type: 'email',
+        type: "email",
         internal: false,
       },
     });
     res.status(200);
-    res.send('Wir haben deine Bewerbung erfolgreich gespeichert.');
+    res.send("Wir haben deine Bewerbung erfolgreich gespeichert.");
   } catch (e) {
     res.status(500);
-    res.send('Beim Erstellen des Tickets ist ein Fehler aufgetreten. Wenn der Fehler noch mal auftritt, solltest du dich an ccs@chaostreff-flensburg.de melden.');
+    res.send(
+      "Beim Erstellen des Tickets ist ein Fehler aufgetreten. Wenn der Fehler noch mal auftritt, solltest du dich an ccs@chaostreff-flensburg.de melden.",
+    );
   }
 });
 
 app.listen(process.env.PORT);
-console.log(`server started on port ${ process.env.PORT }`);
+console.log(`server started on port ${process.env.PORT}`);
--- content/_components/Header.tsx
+++ content/_components/Header.tsx
@@ -1,10 +1,12 @@
-export default ({ ...props }) => (
+export default ({ text }) => (
   <section class="shadow header">
     <nav class="navbar">
-      <a href="/#headline">#CCS</a>
-      <a href="/#uber-den-beirat">Beirat</a>
-      <a href="/#bewerben">Bewerben</a>
-      <a href="/#faq">F.A.Q.</a>
+      <a href="#headline">#CCS</a>
+      <a href={text.header_about_the_advisory_board_link}>
+        {text.header_about_the_advisory_board}
+      </a>
+      <a href={text.header_apply_link}>{text.header_apply}</a>
+      <a href="#faq">F.A.Q.</a>
     </nav>
   </section>
 );
