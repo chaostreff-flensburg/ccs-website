ff81f3ce2346441b6c357bafde461a8ad1dbe8d0
dasUnicorn <hi@lana.coffee>
2025-10-28 16:17:00 +0100

    update data for new semester, set new get to know us dates

--- content/_data/variables.ts
+++ content/_data/variables.ts
@@ -3,9 +3,11 @@ export const APPLY_URL = "https://bewerben.c3fl.de/#/?proposal=ccs-20251";
 
 /* Current Semester,
 used in Header and Text */
-export const CURRENT_SEMESTER = "Sommer Semester 2025";
-export const START_DATE = "01.05.2025";
-export const END_DATE = "25.10.2025";
+export const CURRENT_SEMESTER = "Winter Semester 2025/2026";
+export const START_DATE = "01.11.2025";
+export const END_DATE = "30.04.2026";
+//export const START_DATE = "01.05.2025";
+//export const END_DATE = "25.10.2025";
 
 /* Set to 'true' to display ' we are currently setting up funding' info box on main site */
 export const SHOW_FUNDING_BOX = true;
@@ -16,6 +18,6 @@ export const FUNDING_IS_OUT = false;
 export const NEWSLETTER_URL = "https://newsletter.c3fl.de/subscription/form";
 
 /* Next Dates for "Get to Know Us" Calls: */
-export const GTKU_DATE_ONE = "Do, 21.08.2025 - 19:00 bis 20:00 Uhr";
-export const GTKU_DATE_TWO = "Fr, 26.09.2025 - 19:00 bis 20:00 Uhr";
+export const GTKU_DATE_ONE = "Di, 04.11.2025 - 18:00 bis 19:00 Uhr";
+export const GTKU_DATE_TWO = "Do, 18.12.2025 - 19:00 bis 20:00 Uhr";
 export const GTKU_LINK = "https://meet.ffmuc.net/ccs-info";
