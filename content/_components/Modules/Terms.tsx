d48efc7898f6118e716e2cb764327f3b47f51f60
dasUnicorn <hi@lana.coffee>
2025-11-06 12:52:42 +0100

    update terms

--- content/_components/Modules/Terms.tsx
+++ content/_components/Modules/Terms.tsx
@@ -6,44 +6,57 @@ import {
 } from "../../_data/variables.ts";
 import Button from "../Buttons/Button.tsx";
 
+
 const styles = {
   buttonContainer: {
     display: "flex",
     'justify-content': "center",
   },
+  title: {
+    "font-size": "1.3rem"
+  }
 };
 
 export default ({ text }) => (
   <section>
     <h2 id="scholarhip-terms">{text.terms_title}</h2>
-    <ol>
-      <li>
-        {text.terms_point1a}
-        {CURRENT_SEMESTER}
-        {text.terms_point1b}
-      </li>
-      <li>
-        {text.terms_point11a}
-        {START_DATE}
-        {text.terms_point11b}
-        {END_DATE}
-      </li>
-      <li>{text.terms_point12}</li>
-      <li>{text.terms_point2}</li>
+
+      <h3 style={styles.title}>{text.terms_title1}</h3>
       <ul>
-        <li>{text.terms_point21}</li>
+        <li>{text.terms_point1_dateframe1} {CURRENT_SEMESTER} {text.terms_point1_dateframe2}</li>
+        <li>{text.terms_point2_begin} {START_DATE} {text.terms_point2_and} {END_DATE} {text.terms_point2_end}</li>
+        <li>{text.terms_point3_uni}</li>
+        <li>{text.terms_point4_taxes}</li>
+        <li>{text.terms_point5_profs}</li>
       </ul>
-      <li>{text.terms_point3}</li>
-      <li>{text.terms_point4}</li>
-      <li>{text.terms_point5}</li>
+
+      <h3 style={styles.title}>{text.terms_title2}</h3>
       <ul>
-        <li>{text.terms_point51}</li>
-        <li>{text.terms_point52}</li>
-        <li>{text.terms_point53}</li>
+        <li>{text.terms_point1_theme}</li>
+        <li>{text.terms_point2_area}</li>
+        <li>{text.terms_point3_indipendence}</li>
+        <li>{text.terms_point4_content}</li>
       </ul>
-      <li>{text.terms_point6}</li>
-      <li>{text.terms_point7}</li>
-    </ol>
+
+      <h3 style={styles.title}>{text.terms_title3}</h3>
+      <ul>
+        <li>{text.terms_point1_supply}</li>
+        <li>{text.terms_point2_openAccess_beginn} <a href="test.de">{text.terms_point2_openAccess_link}</a> {text.terms_point2_openAccess_end}</li>
+        <li>{text.terms_point3_fame}</li>
+      </ul>
+
+      <h3 style={styles.title}>{text.terms_title4}</h3>
+      <ul>
+        <li>{text.terms_point1_poster}</li>
+        <li>{text.terms_point2_presentation}</li>
+     </ul>
+
+      <h3 style={styles.title}>{text.terms_title5}</h3>
+      <ul>
+        <li>{text.terms_point1_optional}</li>
+      </ul>
+
+    
     <div style={styles.buttonContainer}>
       <Button link={APPLY_URL}>{text.applyNow_buttonText}</Button>
     </div>
