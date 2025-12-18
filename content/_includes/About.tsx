84275c4c98329170a16fa02da40d21db0380ca79
dasUnicorn <hi@lana.coffee>
2025-12-18 02:29:34 +0100

    add about us page

---
+++ content/_includes/About.tsx
@@ -0,0 +1,34 @@
+export default ({ title, children, ...props }) => (
+  <props.comp.Base title={title} {...props}>
+    <section data-theme="dark" class="layout">
+      <main class="container">
+        <div class="card border-0 shadow rounded">
+          <div class="headerBackground row">
+            <div class="col">
+              <h1>Chaotischer Catalysator Stipendium</h1>
+            </div>
+          </div>
+          <section class="shadow header">
+            <nav class="navbar">
+              <a href="/">Startseite</a>
+              <a href="/about/" class="active">
+                Über uns
+              </a>
+              <a href="/team/">
+                Team
+              </a>
+              <a href="/blog/page/1/">Blog</a>
+              <a href="/publication/page/1/">Publikationen</a>
+            </nav>
+          </section>
+          <div class="card-body content">
+            <main class="p-md-4">{children}</main>
+          </div>
+        </div>
+      </main>
+      <footer class="p-4 container">
+        <props.comp.Footer {...props} />
+      </footer>
+    </section>
+  </props.comp.Base>
+);
---
+++ content/about/page.tsx
@@ -0,0 +1,10 @@
+export const layout = "AboutIndex.mdx";
+
+export default function* () {
+ 
+
+  yield {
+    url: "/about/",
+    layout: "AboutIndex.mdx",
+  };
+}
