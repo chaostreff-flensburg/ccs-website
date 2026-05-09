0cacb81b84744164dedc692dc3fafab9230ed454
dasUnicorn <hi@lana.coffee>
2026-05-10 00:50:49 +0200

    Update apply texts, add info texts for new focus scolarship

--- content/_components/Modules/ApplyNow.tsx
+++ content/_components/Modules/ApplyNow.tsx
@@ -19,7 +19,19 @@ export default ({ text }) => (
       {text.applyNow_info2}
     </p>
     <p class="section">{text.applyNow_text}</p>
-    <div style={styles.buttonContainer}>
+    <div class="display-row ">
+      <div style={"padding: 20px"}>
+        <h3>{text.applyNow_focusTopicTitle}</h3>
+        <p>{text.applyNow_focusTopicInfo}</p>
+        <p>{text.applyNow_focusTopicHint}</p>
+      </div>
+      <div style={"padding: 20px"}>
+        <h3>{text.applyNow_generalTopicTitle}</h3>
+        <p>{text.applyNow_generalTopicInfo}</p>
+        <p>{text.applyNow_generalTopicHint}</p>
+      </div>
+    </div>
+      <div style={styles.buttonContainer}>
       <Button link={APPLY_URL}>{text.applyNow_buttonText}</Button>
     </div>
   </section>
